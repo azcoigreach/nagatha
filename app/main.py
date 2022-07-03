f2c231579d2738c7acc134085eee959a1c09836e
AZcoigreach <azcoigreach@gmail.com>
2022-07-02 20:41:48 -0700

    add battlemetrics

--- app/main.py
+++ app/main.py
@@ -42,63 +42,68 @@ async def on_ready():
 
 
 @bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
+async def add(ctx):
+    """v-rising stats"""
+    await ctx.send(get_battlemetrics_data()['data']['attributes']['name'])
+
+# @bot.command()
+# async def add(ctx, left: int, right: int):
+#     """Adds two numbers together."""
+#     await ctx.send(left + right)
 
 
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
+# @bot.command()
+# async def roll(ctx, dice: str):
+#     """Rolls a dice in NdN format."""
+#     try:
+#         rolls, limit = map(int, dice.split('d'))
+#     except Exception:
+#         await ctx.send('Format has to be in NdN!')
+#         return
 
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
+#     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+#     await ctx.send(result)
 
 
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
+# @bot.command(description='For when you wanna settle the score some other way')
+# async def choose(ctx, *choices: str):
+#     """Chooses between multiple choices."""
+#     await ctx.send(random.choice(choices))
 
 
-@bot.command()
-async def ping(ctx):
-    """Ping pong"""
-    await ctx.send('Hello Dear.')
+# @bot.command()
+# async def repeat(ctx, times: int, content='repeating...'):
+#     """Repeats a message multiple times."""
+#     for i in range(times):
+#         await ctx.send(content)
 
 
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+# @bot.command()
+# async def ping(ctx):
+#     """Ping pong"""
+#     await ctx.send('Hello Dear.')
 
 
-@bot.group()
-async def cool(ctx):
-    """Says if a user is cool.
-
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+# @bot.command()
+# async def joined(ctx, member: discord.Member):
+#     """Says when a member joined."""
+#     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
 
-@cool.command(name='bot')
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send('Yes, the bot is cool.')
+# @bot.group()
+# async def cool(ctx):
+#     """Says if a user is cool.
+
+#     In reality this just checks if a subcommand is being invoked.
+#     """
+#     if ctx.invoked_subcommand is None:
+#         await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+
+# @cool.command(name='bot')
+# async def _bot(ctx):
+#     """Is the bot cool?"""
+#     await ctx.send('Yes, the bot is cool.')
 
 
 bot.run(settings.DISCORD_TOKEN)
